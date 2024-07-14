9678c165b644306ef6849f142df91e3301b77c8c
deepamaria <deepamaria89@gmail.com>
2024-07-14 14:05:39 +0200

    Dropdown component editted with the select tag and a fake array of cities for checking in UI

--- src/components/DropDown.tsx
+++ src/components/DropDown.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 
+const cities = ['New York', 'Jacksonville', 'Washington', 'Dallas', 'Columbus'];
+
 const DropDown: React.FC = () => {
   return (
-    <div>DropDown</div>
+    <div>
+        <select>
+            <option value="">Select city</option>
+            {cities.map(city => (
+                <option key={city} value={city}>{city}</option>
+            ))}
+       </select>
+    </div>
   );
 }
 
